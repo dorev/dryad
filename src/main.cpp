f0c6721eda6468187d99d7470e51eb4772b47e1a
root <root@void.kali>
2019-12-25 10:37:49 -0500

    Progress on Score construction

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,7 @@ namespace
 {
   std::map<std::string, int> noteNumber(
   {
+    {"", -1},
     {"C", 0},
     {"C#", 1},
     {"Db", 1},
@@ -52,11 +53,11 @@ struct Pitch
   NodePtr _nodePtr;
 
   Pitch() 
-    : _step("C")
+    : _step("")
     , _alter(0)
     , _octave(0)
-    , _duration(0)
-    , _num(_octave * (::noteNumber[_step] + _alter))
+    , _duration(-1)
+    , _num(-1)
     , _nodePtr(nullptr)
   {}
 
@@ -69,6 +70,39 @@ struct Pitch
     , _nodePtr(nodePtr)
   {}
 
+  bool fromNode(xml_node& noteNode)
+  {
+    if(!(!strcmp(noteNode.name(),"note") 
+    && noteNode.child("pitch") 
+    && noteNode.child("duration")))
+      return false;
+    
+    if(xml_node step = noteNode.child("pitch").child("step"))
+      _step = step.text().as_string();
+    else 
+      return false;
+    
+    if(xml_node alter = noteNode.child("pitch").child("alter"))
+      _alter = alter.text().as_int();
+    else 
+      return false;
+    
+    if(xml_node octave = noteNode.child("pitch").child("octave"))
+      _octave = octave.text().as_int();
+    else
+      return false;
+
+    _duration = noteNode.child("duration").text().as_int();
+    
+    _num = _octave * (::noteNumber[_step] + _alter);
+
+    _nodePtr = std::make_shared<xml_node>(noteNode);
+
+    return true;
+  }
+
+  
+
   bool operator==(const Pitch& other) const
   {
     return _duration == other._duration && _num == other._num;
@@ -76,7 +110,8 @@ struct Pitch
 
   bool operator<(const Pitch& other) const
   {
-    return _num < other._num || _duration < other._duration;
+    return  _num  < other._num 
+        || (_num == other._num && _duration < other._duration);
   }
 
 };
@@ -103,6 +138,16 @@ struct Score
 {
   std::map<int, ScorePosition> _score;
 
+  Score(xml_document& xmlScore)
+  {
+    uniformizeDivisions(xmlScore);
+    fillScore(xmlScore);
+
+
+  }
+
+  
+
   ScorePositionPtr operator[](int index)
   {
     return (_score.find(index) == _score.end())
@@ -115,61 +160,113 @@ struct Score
     return _score[pos].insert(pitch);
   }
 
-  bool finalize()
+  bool updateResonatingNotes()
   {
-    // normalize divisions
     // add resonating notes
-    // combine tied notes
-
     return true;
   }
 
-};
-
-
-
-void uniformizeDivisions(xml_document& xml)
-{
-  std::map<size_t, int> divisions = {};
-  std::map<size_t, int> divisionFactors = {};
-
-  // Find all division nodes
-  for(auto& division : xml.select_nodes("//divisions"))
-    divisions[division.node().offset_debug()] = std::stoi(division.node().child_value());
-
-  int maxDivision = std::max_element(divisions.begin(), divisions.end())->second;
-  
-  // Set all factors to normalize quarter-note durations
-  for(auto& division : divisions)
-    divisionFactors[division.first] = maxDivision / division.second;
-
-  // Set all division nodes to the same value
-  for(auto& division : xml.select_nodes("//divisions"))
-    division.node().text().set(std::to_string(maxDivision).c_str());
-
-  // Adjust all durations of the document
-  for(auto& duration : xml.select_nodes("//duration"))
+  void fillScore(xml_document& xmlScore)
   {
-    size_t offset = duration.node().offset_debug();
-    int value = std::stoi(duration.node().child_value());
+    for (auto& part : xmlScore.select_nodes("//part"))
+    {
+      int pos = 0;
 
-    // Find modifier to apply
-    auto revItr = std::find_if
-    (
-      divisionFactors.rbegin(), 
-      divisionFactors.rend(), 
-      [&](auto& kv) { return kv.first < offset; }
-    );
+      for (auto& measure : part.node().children())
+      {
+        int shift = 0;
 
-    // Iterator should never reach rend because a 'duration' node
-    // should never be before the first 'divisions' node
-    assert(revItr != divisionFactors.rend());
+        for(auto& node : measure.children())
+        {
+          // Check for position shift
+          if(!strcmp(node.name(),"backup"))
+          {
+            shift -= std::stoi(node.child_value());
+            continue;
+          }
+          if(!strcmp(node.name(),"forward"))
+          {
+            shift += std::stoi(node.child_value());
+            continue;
+          }
+          
+          if(shift)
+          {
+            pos += shift;
+            shift = 0;
+          }
 
-    // Update duration value
-    duration.node().text().set(std::to_string(value * (*revItr).second).c_str());
+          if(strcmp(node.name(),"note"))
+            continue;
+          
+          
+
+
+          if(auto pitch = node.child("pitch"))
+          {
+            // add Pitch ctor from node
+          }
+
+
+
+
+          // pitch
+          // rest
+
+          // duration
+          // chord
+
+
+
+        } // end of for nodes
+      } // end of for measures
+    } // end of for parts
+
   }
-}
 
+  void uniformizeDivisions(xml_document& xml)
+  {
+    std::map<size_t, int> divisions = {};
+    std::map<size_t, int> divisionFactors = {};
+
+    // Find all division nodes
+    for(auto& division : xml.select_nodes("//divisions"))
+      divisions[division.node().offset_debug()] = division.node().text().as_int();
+
+    int maxDivision = std::max_element(divisions.begin(), divisions.end())->second;
+    
+    // Set all factors to normalize quarter-note durations
+    for(auto& division : divisions)
+      divisionFactors[division.first] = maxDivision / division.second;
+
+    // Set all division nodes to the same value
+    for(auto& division : xml.select_nodes("//divisions"))
+      division.node().text().set(std::to_string(maxDivision).c_str());
+
+    // Adjust all durations of the document
+    for(auto& duration : xml.select_nodes("//duration"))
+    {
+      size_t offset = duration.node().offset_debug();
+      int value = duration.node().text().as_int();
+
+      // Find modifier to apply
+      auto revItr = std::find_if
+      (
+        divisionFactors.rbegin(), 
+        divisionFactors.rend(), 
+        [&](auto& kv) { return kv.first < offset; }
+      );
+
+      // Iterator should never reach rend because a 'duration' node
+      // should never be before the first 'divisions' node
+      assert(revItr != divisionFactors.rend());
+
+      // Update duration value
+      duration.node().text().set(std::to_string(value * (*revItr).second).c_str());
+    }
+  }
+
+};
 
 int main()
 {
@@ -191,33 +288,7 @@ int main()
     return 0;
   }
 
-  uniformizeDivisions(xmlScore);
-
-  for (auto& part : xmlScore.select_nodes("//part"))
-  {
-    for (auto& measure : part.node().children())
-    {
-      for(auto& node : measure.children())
-      {
-        if
-        (
-          (!strcmp(node.name(),"backup") || !strcmp(node.name(),"forward"))
-          && node.child("duration")
-        )
-          true; //std::cout << "    " << node.name() << " : " << node.child("duration").child_value() << "\n";
-        
-      } // end of for nodes
-    } // end of for measures
-  } // end of for parts
+  Score score(xmlScore);
 
   return 0;
 }
-
-/*
-
-Scan for backup nodes
-Scan for division nodes
-Parse by parts
-Keep node ptr for indicative styling
-
-*/
