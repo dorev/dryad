a78265bf9b2d4006e0da706559253557ecc108f0
root <root@void.kali>
2020-01-15 16:48:09 -0500

    Setting the scene for chord analysis

--- src/chordAnalysis.h
+++ src/chordAnalysis.h
@@ -4,11 +4,11 @@
 #include "chords.h"
 
 using Interval = std::pair<int, int>;
-using IntervalList = std::set<Interval>;
+using IntervalsList = std::set<Interval>;
 using PitchInterval = std::pair<const Pitch*,const Pitch*>;
-using PitchIntervalList = std::set<PitchInterval>;
+using PitchIntervalsList = std::set<PitchInterval>;
 
-std::set<const Pitch*> allSoundsOfPos(const ScorePosition& pos)
+std::set<const Pitch*> everyPitchInPos(const ScorePosition& pos)
 {
   // Build set of all notes heard at that position
   std::set<const Pitch*> allNotes;
@@ -20,11 +20,11 @@ std::set<const Pitch*> allSoundsOfPos(const ScorePosition& pos)
   return allNotes;
 }
 
-PitchIntervalList findIntervalsInPos(int semitoneInterval, const ScorePosition& pos)
+PitchIntervalsList findIntervalsInPos(int semitoneInterval, const ScorePosition& pos)
 {
-  PitchIntervalList output;
+  PitchIntervalsList output;
 
-  std::set<const Pitch*> allSounds = allSoundsOfPos(pos);
+  std::set<const Pitch*> allSounds = everyPitchInPos(pos);
   
   for(auto& note1 : allSounds)
   {
@@ -41,19 +41,40 @@ PitchIntervalList findIntervalsInPos(int semitoneInterval, const ScorePosition& 
   return output;
 }
 
-// Is that relevant??!?! Chord are unlikely to have 
-// consecutive triads, this is not the good approach
-std::set<IntervalList> findConsecutiveTriads(const ScorePosition& pos)
-{
-  auto minorTriads = findIntervalsInPos(3, pos);
-  auto majorTriads = findIntervalsInPos(4, pos);
 
+IntervalsList findIntervalsInPureNotes(int interval, std::set<int> notes)
+{
+
+  
   return {};
 }
 
-// structure for Chord vs Scale Degree
+std::set<Chord> findChordInPos(const ScorePosition& pos)
+{
+  // how to identify chords qualities
+  
+  // what's the maximum amount of notes 
+  //I want to deal with in a chord (4? 5? more?)
 
-// findChordInPos()
+  // what makes a chord!?
+    // root + fifth or root + third...
+    // let's start with that!!
+
+  std::set<const Pitch*> pitches = everyPitchInPos(pos);
+  std::set<int> pureNotes;
+
+  // Extract pure notes from pitch
+  std::for_each(ALL(pitches), 
+    [&](auto pitch) { pureNotes.insert(pitch->_num % 12); });
+
+  // Find interesting intervals in pure notes
+
+  auto minorTriads = findIntervalsInPureNotes(3, pureNotes);
+  auto majorTriads = findIntervalsInPureNotes(4, pureNotes);
+  auto majorFifths = findIntervalsInPureNotes(7, pureNotes);
+
+  return {};
+}
 
 // degreeOfChordInScale()
 
