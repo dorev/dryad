975eac0288e7e1daa2b056471ee53d7ca78c4d11
root <root@void.kali>
2020-01-16 17:18:54 -0500

    Progress on chord analysis

--- src/chordAnalysis.h
+++ src/chordAnalysis.h
@@ -62,12 +62,27 @@ PitchIntervalsList findPureIntervalsInPos(int searchedInterval, const ScorePosit
       
       int interval = (note1->_num % 12) - (note2->_num % 12);
       
+      // EXPERIMENT
+      // The objective here is to have the first element of the output
+      // pair being the lowest note of the interval, this way, if we're looking
+      // for fifths, pairs returns by this method will always have the root
+      // in the first element of the pair
+      bool flipPair = false;
+
       // Measure the interval as if it was from another octave
       if(interval < 0)
+      {
+        flipPair = true;
         interval += 12; 
+      }
 
       if(interval == searchedInterval)
-        result.emplace(note1, note2);
+      {
+        if(flipPair)
+          result.emplace(note2, note1);
+        else
+          result.emplace(note1, note2);
+      }
     }
   }
   
@@ -78,21 +93,52 @@ PitchIntervalsList findPureIntervalsInPos(int searchedInterval, const ScorePosit
 
 std::set<Chord> findChordInPos(const ScorePosition& pos)
 {
-  // Find interesting intervals in pure notes
+  // Quick escape
+  if(pos._notes.size() < 2)
+    return {};
 
+  // Find interesting pure intervals
   auto minorTriads = findPureIntervalsInPos(3, pos);
   auto majorTriads = findPureIntervalsInPos(4, pos);
-  auto majorFifths = findPureIntervalsInPos(7, pos);
+  auto fifths = findPureIntervalsInPos(7, pos);
 
-  // For every major 5th, look if we have a connected triad
+  // For every fifth, look if we have a connected triad
+  for(auto& fifth : fifths)
+  {
     // A triad connected to the lowest note is 100% a chord
-      // Look for triad above 5th to find seventh
+    const auto minorTriadItr = std::find_if(ALL(minorTriads), 
+      [&](PitchInterval& triad){ return triad.first == fifth.first; });
     
-    // Look for a major fourth of the lowest note for sus4
+    const auto majorTriadItr = std::find_if(ALL(majorTriads), 
+      [&](PitchInterval& triad){ return triad.first == fifth.first; });
+    
+    const bool minorTriadFound = minorTriadItr == minorTriads.end();
+    const bool majorTriadFound = majorTriadItr == majorTriads.end();
+
+    if(!minorTriadFound && !majorTriadFound) 
+      continue;
+
+    if(minorTriadFound && majorTriadFound)
+      { /* Let's handle jazz stuff later */ }
+  
+      // Look for triad above fifth to find seventh
+    
+    // Look for a fourth of the lowest note for sus4
     // Look for a major second of the lowest note for sus2
     // Look for a major second of the highest note to find a 6 chord
+  }
 
-  
+  if(fifths.size() != 0)
+  {
+
+  }
+
+  // If we have no fifth
+    // Look for degrees sequence that might imply a chord
+      // 1-3-7/M7
+      // 3-5-7/M7
+      // 1-3
+      // 1-4
     
 
   return {};
